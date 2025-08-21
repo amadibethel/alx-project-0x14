cb0d8b5449fa9afc2007bc65ce60efe0b13e45cb
Bethel Amadi <amadibethel@icloud.com>
2025-08-21 17:45:38 +0200

    Add updated file to Movie App

--- alx-movie-app/src/pages/_document.tsx
+++ alx-movie-app/src/pages/_document.tsx
@@ -1,9 +1,22 @@
+// pages/_document.tsx
+
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Link to PWA manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Theme color for browser toolbar */}
+        <meta name="theme-color" content="#0070f3" />
+
+        {/* Optional: icons can also be linked here */}
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="310x310" href="/icons/ms-icon-310x310.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
